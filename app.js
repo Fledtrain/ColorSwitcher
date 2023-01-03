1f41b546eb4d9dbd73b1fda1621d1bbd807fe9d9
Fledtrain <Joeyew2014@gmail.com>
2023-01-02 23:29:13 -0500

    Commented styling page and fixed button

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 document.querySelector(".color-switcher-btn").onclick = () => {
   document.querySelector(".color-switcher").classList.toggle("active");
 };
+//Onclick of the color-switcher-btn, the color-switcher class will toggle the active class
 
+
+//Grabs all the buttons with the class of theme-buttons
 let themeButtons = document.querySelectorAll(".theme-buttons");
 
+//Loops through the buttons and adds an event listener to each one
+//When the button is clicked, it grabs the data-color attribute and sets it as the value of the --main--color variable
 themeButtons.forEach((color) => {
   color.addEventListener("click", () => {
     let dataColor = color.getAttribute("data-color");
-    document
-      .querySelector(":root")
-      .style.setProperty("--main-color", dataColor);
+    document.querySelector(":root").style.setProperty("--main--color", dataColor);
   });
 });
